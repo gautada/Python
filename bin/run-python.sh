16e326007905c8dceb82c909c33b05df54bec5a4
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2024-01-03 19:31:50 -0500

    Update the run script to deploy python scripts to the system

--- bin/run-python.sh
+++ bin/run-python.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "This script should find/locate a python script and run it passing all values.
+# This script should find/locate a python script and run it passing all values.
 
 # Check that the environment is active
 # This script should not be called directly but should be called from a symlink, the symlink
@@ -8,4 +8,40 @@ echo "This script should find/locate a python script and run it passing all valu
 # Should pass all parameters to the script
 # locate the python script by first searching the public folder then private folder.
 
+BASE_SCRIPT="$(basename "$0")"
+if [ "$BASE_SCRIPT" = "run-python.sh" ]; then
+ echo "[ERROR] Script should be called through a symlink names for the python script to run"
+ exit 1
+fi
 
+BASE_PATH="$HOME/Workspace/Python"
+PUBLIC_PATH="$BASE_PATH/public"
+PRIVATE_PATH="$BASE_PATH/private"
+
+SCRIPT="$BASE_SCRIPT.py"
+PUBLIC_SCRIPT="$PUBLIC_PATH/$SCRIPT"
+PRIVATE_SCRIPT="$PRIVATE_PATH/$SCRIPT"
+
+# echo $PUBLIC_SCRIPT
+
+if [ -f "$PUBLIC_SCRIPT" ]; then
+ SCRIPT="$PUBLIC_SCRIPT"
+else
+ if [ -f "$PRIVATE_SCRIPT" ]; then
+  SCRIPT="$PRIVATE_SCRIPT"
+ else
+  echo "[ERROR] Script ($SCRIPT) not found."
+  exit 2
+ fi
+fi
+
+# echo $SCRIPT
+RTN=0 
+if [ -f "$SCRIPT" ]; then
+ RUN="$(which python) $SCRIPT $@"
+ # echo "RUN: $RUN"
+ eval "$RUN"
+ RTN=$?
+fi
+
+exit $RTN
